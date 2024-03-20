98df607e74dd020f9feb4af41cf3493354ff515f
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-03-20 18:15:12 -0500

    Se hace login del moderator

--- NearbyEats-B/src/main/java/co/edu/uniquindio/proyecto/repository/ModeratorRepo.java
+++ NearbyEats-B/src/main/java/co/edu/uniquindio/proyecto/repository/ModeratorRepo.java
@@ -1,9 +1,13 @@
 package co.edu.uniquindio.proyecto.repository;
 
+import co.edu.uniquindio.proyecto.model.documents.Client;
 import co.edu.uniquindio.proyecto.model.documents.Moderator;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ModeratorRepo extends MongoRepository<Moderator, String> {
+    Optional<Moderator> findByNickname(String nickname);
 }
--- NearbyEats-B/src/main/java/co/edu/uniquindio/proyecto/services/implementations/ModeratorServiceImpl.java
+++ NearbyEats-B/src/main/java/co/edu/uniquindio/proyecto/services/implementations/ModeratorServiceImpl.java
@@ -4,18 +4,33 @@ import co.edu.uniquindio.proyecto.dto.moderator.ModeratorChangePasswordDTO;
 import co.edu.uniquindio.proyecto.dto.moderator.ModeratorLoginDTO;
 import co.edu.uniquindio.proyecto.exceptions.moderator.ModeratorChangePassworException;
 import co.edu.uniquindio.proyecto.exceptions.moderator.ModeratorLoginException;
+import co.edu.uniquindio.proyecto.model.documents.Moderator;
+import co.edu.uniquindio.proyecto.repository.ModeratorRepo;
 import co.edu.uniquindio.proyecto.services.interfaces.ModeratorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional
 @Service
 @RequiredArgsConstructor
 public class ModeratorServiceImpl implements ModeratorService {
+
+    private final ModeratorRepo moderatorRepo;
     @Override
     public void login(ModeratorLoginDTO moderatorLoginDTO) throws ModeratorLoginException {
 
+        Optional<Moderator> moderatorOptional = moderatorRepo.findByNickname(moderatorLoginDTO.nickname());
+
+        if (moderatorOptional.isEmpty()){
+            throw new ModeratorLoginException("El nickName no existe");
+        }
+
+        if (!moderatorOptional.get().getPassword().equals(moderatorLoginDTO.password())){
+            throw new ModeratorLoginException("");
+        }
     }
 
     @Override
